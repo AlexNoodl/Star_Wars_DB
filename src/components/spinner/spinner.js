7dff8849970819eed514bc6e2e7969239440c1f3
Alexander <alex.noodl@gmail.com>
2020-06-18 20:54:40 +0300

    0.1 Компоненты Планеты, список персонажей и выбор персонажа

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+import "./spinner.css";
+
+const Spinner = () => {
+   return (
+      <div className="loadingio-spinner-double-ring-npmfbti91qf">
+         <div className="ldio-t7zye30cd6">
+            <div></div>
+            <div></div>
+            <div>
+               <div></div>
+            </div>
+            <div>
+               <div></div>
+            </div>
+         </div>
+      </div>
+   );
+};
+
+export default Spinner;
